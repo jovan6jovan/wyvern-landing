05930991aa18764aa3ffe92f3c9b791d0a56d573
jovan6jovan <jovan6jovanovic@gmail.com>
2020-02-08 02:59:30 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// NAVIGATION
+let mainNav = document.querySelector("#js-menu");
+let navBarToggle = document.querySelector("#js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
